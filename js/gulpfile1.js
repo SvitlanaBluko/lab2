7f02060d76fe2d28491321bb593c04bd2f09da1f
SvitlanaBluko <99684470+SvitlanaBluko@users.noreply.github.com>
2024-01-07 19:29:14 +0200

    Add files via upload

---
+++ js/gulpfile1.js
@@ -0,0 +1,23 @@
+const { src, dest, series, parallel } = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const cleanCSS = require('gulp-clean-css');
+const rename = require("gulp-rename");
+
+function buildDev() {
+  return src('style1.scss')
+    .pipe(sourcemaps.init()) 
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(dest('dev'));
+}
+
+function buildProd() {
+  return src('style1.scss')
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(dest('prod'));  
+}
+
+exports.default = series(parallel(buildDev, buildProd));
